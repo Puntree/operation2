bde1508260a780996ef6c8e361abaf9521030d07
Puntree <88648330+Puntree@users.noreply.github.com>
2021-08-14 15:33:57 +0700

    Add files via upload

---
+++ บวกลบคูณหาร2.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	double a,b;
+	printf("First Number : ");
+	scanf("%lf",&a);
+	printf("Second Number : ");
+	scanf("%lf",&b);
+	printf("sum = %lf\n", a+b);
+	printf("diff = %lf\n", a-b);
+	printf("mul = %lf\n", a*b);
+	printf("div = %lf\n", a/b);
+	return 0;
+}
+
